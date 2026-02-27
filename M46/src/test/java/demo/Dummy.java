ef4159d38d2d61f237727212251fc2dbf1812939
amruta <raskondaamruta32@gamil.com>
2026-02-27 11:43:20 +0530

    commit

---
+++ M46/src/test/java/demo/Dummy.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helooo");
+		System.out.println("helooo");
+		System.out.println("helooo");
+	}
+
+}
